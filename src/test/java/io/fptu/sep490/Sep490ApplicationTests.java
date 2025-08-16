08707e0d0d2c0bb4fba6e363f5134dad28008f66
conghieu <=>
2025-08-16 10:06:24 +0700

    init

---
+++ src/test/java/io/fptu/sep490/Sep490ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fptu.sep490;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep490ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
